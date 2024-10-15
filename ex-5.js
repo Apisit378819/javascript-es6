4cb4125d939ac2b3edbd84688c38ec2d3c2a5ac8
Tonkla <Apisit378819@gmail.com>
2024-10-15 14:48:17 +0700

    add ex1-ex7

--- ex-5.js
+++ ex-5.js
@@ -1,9 +1,14 @@
-function sum() {
-  // Start coding here !
+function sum (...numbers) {
+  let result =  numbers.reduce((acc,crr) => acc + crr , 0)
+  return result
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
 let result2 = sum(1, 2, 3, 4);
 
+
+
+
 console.log(`Result ${result1}`);
 console.log(`Result ${result2}`);
+
